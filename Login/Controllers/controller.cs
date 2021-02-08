a62308b09742d85973a6e3e0c2a36b7abb0d00f3
unknown <you@example.com>
2021-02-08 08:01:52 +0100

    first commit

---
+++ Login/Controllers/controller.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Login
+{
+    public class controller
+    {
+
+        #region Eigenschaften
+        #endregion
+
+        #region Accessoren/Modifier
+        #endregion
+
+        #region Konstruktoren
+        #endregion
+
+        #region Worker
+        public string ueberpruefen(string text1, string text2)
+        {
+            Login user = new Login(-1,text1,text2,"");
+            user.ueberpruefenInAPI();
+            string index = user.ueberpruefenInAPI();
+            
+            return index;
+        }
+        #endregion
+      
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# Lernsituation_3
